b3123d967089dc18c176f552af1509308ae0e5e2
mauroservienti <mauro.servienti@gmail.com>
2020-05-13 13:49:22 +0200

    RavenDB reader skeleton

---
+++ src/TimeoutMigrationTool/RavenDB/RavenDBTimeoutsReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Particular.TimeoutMigrationTool.RavenDB
+{
+    class RavenDBTimeoutsReader
+    {
+        public Task<string[]> ListEndpoints(string connectionString, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<TimeoutData>> ReadTimeoutsFrom(string connectionString, CancellationToken cancellationToken)
+        {
+            //this needs to be paged so Task<List<TimeoutData>> is not enough
+            throw new NotImplementedException();
+        }
+    }
+}
