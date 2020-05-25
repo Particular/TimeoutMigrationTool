759a1338162957c23170b853a17bc6dc66a23067
WilliamBZA <william.brander@gmail.com>
2020-05-25 14:46:23 +0200

    Prepare data for migration

--- src/TimeoutMigrationTool/SqlDialect.cs
+++ src/TimeoutMigrationTool/SqlDialect.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Data.Common;
+using System.Threading.Tasks;
 
 namespace Particular.TimeoutMigrationTool
 {
@@ -12,6 +13,8 @@ namespace Particular.TimeoutMigrationTool
         {
             return new MsSqlServer();
         }
+
+        public abstract string GetScriptToPrepareTimeouts(string originalTableName, int batchSize);
     }
 
     public class MsSqlServer : SqlDialect
@@ -23,11 +26,64 @@ namespace Particular.TimeoutMigrationTool
 
             return connection;
         }
+
+        public override string GetScriptToPrepareTimeouts(string originalTableName, int batchSize)
+        {
+            return $@"
+BEGIN TRANSACTION
+
+    CREATE TABLE MigrationState (
+        Status VARCHAR(15) NOT NULL,
+        Batches INT NOT NULL,
+        RunParameters NVARCHAR(MAX)
+    );
+
+    CREATE TABLE [{originalTableName}_migration] (
+        Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+        BatchNumber INT,
+        Status INT, /* NULL = prepared, 1 = staged, 2 = migrated */
+        Destination NVARCHAR(200),
+        SagaId UNIQUEIDENTIFIER,
+        State VARBINARY(MAX),
+        Time DATETIME,
+        Headers NVARCHAR(MAX) NOT NULL,
+        PersistenceVersion VARCHAR(23) NOT NULL
+    );
+
+    DELETE [{originalTableName}]
+    OUTPUT DELETED.Id,
+        -1,
+        NULL,
+        DELETED.Destination,
+        DELETED.SagaId,
+        DELETED.State,
+        DELETED.Time,
+        DELETED.Headers,
+        DELETED.PersistenceVersion
+    INTO [{originalTableName}_migration]
+    WHERE [{originalTableName}].Time <= @maxCutOff;
+
+    UPDATE BatchMigration
+    SET BatchMigration.BatchNumber = BatchMigration.CalculatedBatchNumber
+    FROM (
+        SELECT BatchNumber, ROW_NUMBER() OVER (ORDER BY (select 0)) / {batchSize} AS CalculatedBatchNumber
+        FROM [{originalTableName}_migration]
+    ) BatchMigration;
+
+    INSERT INTO MigrationState VALUES ('Prepared', (SELECT COUNT(DISTINCT BatchNumber) from [{originalTableName}_migration]), @RunParameters);
+        
+COMMIT;";
+        }
     }
 
     public class Oracle : SqlDialect
     {
         public override DbConnection Connect(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetScriptToPrepareTimeouts(string originalTableName, int batchSize)
         {
             throw new NotImplementedException();
         }
@@ -39,11 +95,21 @@ namespace Particular.TimeoutMigrationTool
         {
             throw new NotImplementedException();
         }
+
+        public override string GetScriptToPrepareTimeouts(string originalTableName, int batchSize)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class PostgreSql : SqlDialect
     {
         public override DbConnection Connect(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetScriptToPrepareTimeouts(string originalTableName, int batchSize)
         {
             throw new NotImplementedException();
         }
--- src/TimeoutMigrationTool/SqlP/SqlTimeoutStorage.cs
+++ src/TimeoutMigrationTool/SqlP/SqlTimeoutStorage.cs
@@ -1,7 +1,6 @@
-﻿using System;
-
-namespace Particular.TimeoutMigrationTool.SqlP
+﻿namespace Particular.TimeoutMigrationTool.SqlP
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
@@ -9,7 +8,7 @@ namespace Particular.TimeoutMigrationTool.SqlP
     {
         public SqlTimeoutStorage(string sourceConnectionString, SqlDialect dialect, string timeoutTableName)
         {
-            this.sourceConnectionString = sourceConnectionString;
+            connectionString = sourceConnectionString;
             this.dialect = dialect;
             this.timeoutTableName = timeoutTableName;
         }
@@ -19,9 +18,27 @@ namespace Particular.TimeoutMigrationTool.SqlP
             throw new System.NotImplementedException();
         }
 
-        public Task<List<BatchInfo>> Prepare(DateTime maxCutoffTime)
+        public async Task<List<BatchInfo>> Prepare(DateTime maxCutoffTime)
         {
-            throw new System.NotImplementedException();
+            using (var connection = dialect.Connect(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = dialect.GetScriptToPrepareTimeouts(timeoutTableName, 5);
+
+                var cutOffParameter = command.CreateParameter();
+                cutOffParameter.ParameterName = "maxCutOff";
+                cutOffParameter.Value = maxCutoffTime;
+                command.Parameters.Add(cutOffParameter);
+
+                var runParametersParameter = command.CreateParameter();
+                runParametersParameter.ParameterName = "RunParameters";
+                runParametersParameter.Value = "...where do I get this from?";
+                command.Parameters.Add(runParametersParameter);
+
+                await command.ExecuteNonQueryAsync().ConfigureAwait(false);
+
+                return null;
+            }
         }
 
         public Task<List<TimeoutData>> ReadBatch(int batchNumber)
@@ -49,7 +66,7 @@ namespace Particular.TimeoutMigrationTool.SqlP
             throw new NotImplementedException();
         }
 
-        readonly string sourceConnectionString;
+        readonly string connectionString;
         readonly SqlDialect dialect;
         readonly string timeoutTableName;
     }
