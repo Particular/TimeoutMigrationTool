04cfb09613f49fba755a0b7b6d8d351edfcacf78
Andreas Öhlund <andreas.ohlund@particular.net>
2020-06-22 10:28:25 +0200

    Fix filename

--- src/TimeoutMigrationTool/ICreateTransportTimeouts.cs
+++ src/TimeoutMigrationTool/ICreateTransportTimeouts.cs
@@ -1,7 +1,6 @@
 ﻿namespace Particular.TimeoutMigrationTool
 {
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     public interface ICreateTransportTimeouts
@@ -9,18 +8,5 @@
         Task<int> StageBatch(List<TimeoutData> timeouts);
         Task<int> CompleteBatch(int number);
         Task<MigrationCheckResult> AbleToMigrate(EndpointInfo endpoint);
-    }
-
-    public class MigrationCheckResult
-    {
-        public List<string> Problems { get; set; } = new List<string>();
-
-        public bool CanMigrate
-        {
-            get
-            {
-                return !Problems.Any();
-            }
-        }
     }
 }
---
+++ src/TimeoutMigrationTool/MigrationCheckResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Particular.TimeoutMigrationTool
+{
+    using System.Collections.Generic;
+
+    public class MigrationCheckResult
+    {
+        public List<string> Problems { get; set; } = new List<string>();
+
+        public bool CanMigrate
+        {
+            get
+            {
+                return !Problems.Any();
+            }
+        }
+    }
+}
