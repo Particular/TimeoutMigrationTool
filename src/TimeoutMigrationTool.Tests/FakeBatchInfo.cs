c80987e031802cd1420b80c1b783a70989bd47a4
Andreas Öhlund <andreas.ohlund@particular.net>
2020-06-23 12:19:14 +0200

    Fix file

---
+++ src/TimeoutMigrationTool.Tests/FakeBatchInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace TimeoutMigrationTool.Tests
+{
+    using System;
+    using Particular.TimeoutMigrationTool;
+
+    public class FakeBatchInfo : BatchInfo
+    {
+        public FakeBatchInfo(int number, BatchState state, int numberOfTimeouts) : base(number, state, numberOfTimeouts)
+        {
+
+        }
+
+        public string[] TimeoutIds { get; set; }
+        public DateTime CutoffDate { get; set; }
+        public string EndpointName { get; set; }
+
+        public void MarkAsCompleted()
+        {
+            State = BatchState.Completed;
+        }
+
+        public void MarkAsStaged()
+        {
+            State = BatchState.Staged;
+        }
+    }
+}
--- src/TimeoutMigrationTool.Tests/FakeToolState.cs
+++ src/TimeoutMigrationTool.Tests/FakeToolState.cs
@@ -1,6 +1,5 @@
 ﻿namespace TimeoutMigrationTool.Tests
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
@@ -39,29 +38,6 @@
             var pendingBatch = Batches.First(x => x.State == BatchState.Pending);
 
             return Task.FromResult<BatchInfo>(pendingBatch);
-        }
-    }
-
-
-    public class FakeBatchInfo : BatchInfo
-    {
-        public FakeBatchInfo(int number, BatchState state, int numberOfTimeouts) : base(number, state, numberOfTimeouts)
-        {
-
-        }
-
-        public string[] TimeoutIds { get; set; }
-        public DateTime CutoffDate { get; set; }
-        public string EndpointName { get; set; }
-
-        public void MarkAsCompleted()
-        {
-            State = BatchState.Completed;
-        }
-
-        public void MarkAsStaged()
-        {
-            State = BatchState.Staged;
         }
     }
 }
