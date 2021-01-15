eaabe7bf9baf2e0cb728fb3685e969d035d436d3
danielmarbach <daniel.marbach@openplace.net>
2021-01-15 13:32:35 +0100

    Wire up staging table creation

--- src/TimeoutMigrationTool/SqlT/SqlTQueueCreator.cs
+++ src/TimeoutMigrationTool/SqlT/SqlTQueueCreator.cs
@@ -4,9 +4,9 @@
     using System.Threading.Tasks;
     using Microsoft.Data.SqlClient;
 
-    public class SqlTQueueCreator
+    public static class SqlTQueueCreator
     {
-        public async Task CreateStagingQueue(SqlConnection connection, string tableName, string databaseName)
+        public static async Task CreateStagingQueue(SqlConnection connection, string tableName, string databaseName)
         {
             await using var transaction = connection.BeginTransaction();
             var sql = string.Format(SqlConstants.CreateDelayedMessageStoreText, tableName, databaseName);
--- src/TimeoutMigrationTool/SqlT/SqlTTimeoutCreator.cs
+++ src/TimeoutMigrationTool/SqlT/SqlTTimeoutCreator.cs
@@ -1,10 +1,21 @@
 ﻿namespace Particular.TimeoutMigrationTool.SqlT
 {
+    using System;
     using System.Collections.Generic;
+    using System.Data;
     using System.Threading.Tasks;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Logging;
 
     public class SqlTTimeoutCreator : ICreateTransportTimeouts
     {
+        public SqlTTimeoutCreator(ILogger logger, string connectionString)
+        {
+            this.logger = logger;
+            this.connectionString = connectionString;
+            connection = new SqlConnection(connectionString);
+        }
+
         public Task<int> StageBatch(List<TimeoutData> timeouts)
         {
             throw new System.NotImplementedException();
@@ -15,9 +26,38 @@
             throw new System.NotImplementedException();
         }
 
-        public Task<MigrationCheckResult> AbleToMigrate(EndpointInfo endpoint)
+        public async Task<MigrationCheckResult> AbleToMigrate(EndpointInfo endpoint)
         {
+            var migrationCheckResult = new MigrationCheckResult();
 
+            if (connection.State != ConnectionState.Open)
+            {
+                try
+                {
+                    await connection.OpenAsync().ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    migrationCheckResult.Problems.Add($"Unable to connect to the server or database under connection string '{connectionString}'. The following exception occured: {e.Message}");
+                    return migrationCheckResult;
+                }
+            }
+
+            try
+            {
+                await SqlTQueueCreator.CreateStagingQueue(connection, timeoutmigrationStagingTable, "databaseName");
+            }
+            catch (Exception e)
+            {
+                migrationCheckResult.Problems.Add($"Unable to create the staging queue '{timeoutmigrationStagingTable}'. The following exception occured: {e.Message}");
+            }
+
+            return migrationCheckResult;
         }
+
+        private readonly SqlConnection connection;
+        private string connectionString;
+        private readonly ILogger logger;
+        private readonly string timeoutmigrationStagingTable = "timeoutmigrationtool-staging-table";
     }
 }
