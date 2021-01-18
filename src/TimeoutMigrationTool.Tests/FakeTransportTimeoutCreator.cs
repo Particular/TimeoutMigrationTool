7b2ec41d45e5ca24346b20148166f6f99f7c7332
danielmarbach <daniel.marbach@openplace.net>
2021-01-18 09:36:16 +0100

    FakeTransportTimeoutCreator cleanup

--- src/TimeoutMigrationTool.Tests/FakeTransportTimeoutCreator.cs
+++ src/TimeoutMigrationTool.Tests/FakeTransportTimeoutCreator.cs
@@ -7,7 +7,7 @@ namespace TimeoutMigrationTool.Tests
     public class FakeTransportTimeoutCreator : ICreateTransportTimeouts
     {
         private List<string> problemsToReturn;
-        public bool BatchWasStaged { get; private set; } = false;
+        public bool BatchWasStaged { get; private set; }
         public List<TimeoutData> TimeoutsStaged { get; } = new List<TimeoutData>();
         public List<int> BatchesCompleted { get; } = new List<int>();
         public bool EndpointWasVerified { get; private set; } = false;
@@ -28,7 +28,7 @@ namespace TimeoutMigrationTool.Tests
         public Task<MigrationCheckResult> AbleToMigrate(EndpointInfo endpoint)
         {
             EndpointWasVerified = true;
-            var problems = this.problemsToReturn ?? new List<string>();
+            var problems = problemsToReturn ?? new List<string>();
 
             var result = new MigrationCheckResult
             {
