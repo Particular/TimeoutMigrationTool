cc23579e3655a1cc768dafc1bde3a686fcb6296e
danielmarbach <daniel.marbach@openplace.net>
2021-01-19 10:09:03 +0100

    Remove unneeded stuff

--- src/TimeoutMigrationTool/SqlT/SqlConstants.cs
+++ src/TimeoutMigrationTool/SqlT/SqlConstants.cs
@@ -2,54 +2,6 @@
 {
     static class SqlConstants
     {
-        public static readonly string StoreDelayedMessageText =
-@"
-DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
-IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON'
-SET NOCOUNT ON;
-
-DECLARE @DueAfter DATETIME = GETUTCDATE();
-SET @DueAfter = DATEADD(ms, @DueAfterMilliseconds, @DueAfter);
-SET @DueAfter = DATEADD(s, @DueAfterSeconds, @DueAfter);
-SET @DueAfter = DATEADD(n, @DueAfterMinutes, @DueAfter);
-SET @DueAfter = DATEADD(hh, @DueAfterHours, @DueAfter);
-SET @DueAfter = DATEADD(d, @DueAfterDays, @DueAfter);
-
-INSERT INTO {0} (
-    Headers,
-    Body,
-    Due)
-VALUES (
-    @Headers,
-    @Body,
-    @DueAfter);
-
-IF(@NOCOUNT = 'ON') SET NOCOUNT ON;
-IF(@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
-
- public static readonly string MoveDueDelayedMessageText = @"
-DECLARE @NOCOUNT VARCHAR(3) = 'OFF';
-IF ( (512 & @@OPTIONS) = 512 ) SET @NOCOUNT = 'ON';
-SET NOCOUNT ON;
-
-WITH message AS (
-    SELECT TOP(@BatchSize) *
-    FROM {0} WITH (UPDLOCK, READPAST, ROWLOCK)
-    WHERE Due < GETUTCDATE())
-DELETE FROM message
-OUTPUT
-    NEWID(),
-    NULL,
-    NULL,
-    1,
-    NULL,
-    deleted.Headers,
-    deleted.Body
-INTO {1};
-
-IF (@NOCOUNT = 'ON') SET NOCOUNT ON;
-IF (@NOCOUNT = 'OFF') SET NOCOUNT OFF;";
-
         public static readonly string DelayedMessageStoreExistsText = @"
    SELECT COUNT(*)
    FROM INFORMATION_SCHEMA.TABLES
