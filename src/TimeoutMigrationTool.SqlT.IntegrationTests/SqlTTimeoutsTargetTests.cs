420bb240a07ccb7cb4923d10626e72bfca29c852
lailabougria <laila.bougria@particular.net>
2021-01-22 12:22:43 +0100

    Add test for non-empty staging table and moved complete tests into sqltimeoutstargettests

--- src/TimeoutMigrationTool.SqlT.IntegrationTests/SqlTTimeoutsTargetTests.cs
+++ src/TimeoutMigrationTool.SqlT.IntegrationTests/SqlTTimeoutsTargetTests.cs
@@ -20,6 +20,7 @@ namespace TimeoutMigrationTool.SqlT.IntegrationTests
 
         string ExistingEndpointName = "ExistingEndpointName";
         string ExistingDestination = "ExistingEndpointNameDirect";
+        private string schema = "dbo";
 
         [SetUp]
         public async Task SetUp()
@@ -40,7 +41,7 @@ namespace TimeoutMigrationTool.SqlT.IntegrationTests
         [Test]
         public async Task AbleToMigrate_delayed_delivery_does_exist_should_indicate_no_problems()
         {
-            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, "dbo");
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
 
             await using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
@@ -49,7 +50,7 @@ namespace TimeoutMigrationTool.SqlT.IntegrationTests
 CREATE TABLE [{1}].[{0}] (
     RowVersion bigint IDENTITY(1,1) NOT NULL
 );
-", $"{ExistingEndpointName}.Delayed", "dbo");
+", $"{ExistingEndpointName}.Delayed", schema);
             await command.ExecuteNonQueryAsync();
 
             var info = new EndpointInfo
@@ -70,7 +71,7 @@ CREATE TABLE [{1}].[{0}] (
         [Test]
         public async Task AbleToMigrate_delayed_delivery_does_not_exist_should_indicate_problems()
         {
-            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, "dbo");
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
 
             await using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
@@ -78,7 +79,7 @@ CREATE TABLE [{1}].[{0}] (
             command.CommandText = string.Format(@"
 IF OBJECT_ID('{0}.{1}', 'u') IS NOT NULL
   DROP TABLE {0}.{1};
-", $"{ExistingEndpointName}.Delayed", "dbo");
+", $"{ExistingEndpointName}.Delayed", schema);
             await command.ExecuteNonQueryAsync();
 
             var info = new EndpointInfo
@@ -99,7 +100,7 @@ IF OBJECT_ID('{0}.{1}', 'u') IS NOT NULL
         [Test]
         public async Task Should_delete_staging_queue_when_aborting()
         {
-            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, "dbo");
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
             var endpointName = "FakeEndpoint";
             await using var endpointTarget = await sut.Migrate(endpointName);
             await sut.Abort(endpointName);
@@ -111,7 +112,7 @@ IF OBJECT_ID('{0}.{1}', 'u') IS NOT NULL
    SELECT COUNT(*)
    FROM INFORMATION_SCHEMA.TABLES
    WHERE TABLE_SCHEMA = '{1}' AND TABLE_NAME = '{0}' AND TABLE_CATALOG = '{2}'
-", "timeoutmigrationtoolstagingtable", "dbo", databaseName);
+", SqlConstants.TimeoutMigrationStagingTable, schema, databaseName);
             var result = await command.ExecuteScalarAsync() as int?;
 
             Assert.That(Convert.ToBoolean(result), Is.False);
@@ -122,7 +123,7 @@ IF OBJECT_ID('{0}.{1}', 'u') IS NOT NULL
         {
             var endpointDelayedTableName = $"{ExistingEndpointName}.Delayed";
 
-            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, "dbo");
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
 
             await using var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
@@ -135,7 +136,7 @@ CREATE TABLE [{1}].[{0}] (
     Due datetime NOT NULL,
     RowVersion bigint IDENTITY(1,1) NOT NULL
 );
-", endpointDelayedTableName, "dbo");
+", endpointDelayedTableName, schema);
             await command.ExecuteNonQueryAsync();
 
             const int BatchNumber = 2;
@@ -169,15 +170,59 @@ CREATE TABLE [{1}].[{0}] (
             await endpointTarget.CompleteBatch(BatchNumber);
 
             var endpointDelayedTableDataTable = new DataTable();
-            using var endpointDelayedTableDataAdapter = new SqlDataAdapter(string.Format("SELECT * FROM [{1}].[{0}]", endpointDelayedTableName, "dbo"), connection);
+            using var endpointDelayedTableDataAdapter = new SqlDataAdapter(string.Format("SELECT * FROM [{1}].[{0}]", endpointDelayedTableName, schema), connection);
             endpointDelayedTableDataAdapter.Fill(endpointDelayedTableDataTable);
 
             var migrationTableDataTable = new DataTable();
-            using var migrationTableDataAdapter = new SqlDataAdapter(string.Format("SELECT * FROM [{1}].[{0}]", "timeoutmigrationtoolstagingtable", "dbo"), connection);
+            using var migrationTableDataAdapter = new SqlDataAdapter(string.Format("SELECT * FROM [{1}].[{0}]", SqlConstants.TimeoutMigrationStagingTable, schema), connection);
             migrationTableDataAdapter.Fill(migrationTableDataTable);
 
             Approver.Verify(endpointDelayedTableDataTable.Rows.OfType<DataRow>().SelectMany(r => r.ItemArray.Take(3)));
             Assert.IsEmpty(migrationTableDataTable.Rows);
+        }
+
+        [Test]
+        public async Task Should_delete_staging_table_when_empty()
+        {
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
+
+            await using var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            await SqlTQueueCreator.CreateStagingQueue(connection, SqlConstants.TimeoutMigrationStagingTable, schema, connection.Database, preview: false);
+
+            await sut.Complete(ExistingEndpointName);
+
+            var sqlStatement = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = '{SqlConstants.TimeoutMigrationStagingTable}' AND TABLE_CATALOG = '{databaseName}'";
+            await using var command = new SqlCommand(sqlStatement, connection)
+            {
+                CommandType = CommandType.Text
+            };
+            var result = await command.ExecuteScalarAsync().ConfigureAwait(false) as int?;
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task Should_throw_when_staging_table_not_empty()
+        {
+            var sut = new SqlTTimeoutsTarget(new TestLoggingAdapter(), connectionString, schema);
+
+            await using var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+            await SqlTQueueCreator.CreateStagingQueue(connection, SqlConstants.TimeoutMigrationStagingTable, schema, connection.Database, preview: false);
+
+            var sql =
+                $"INSERT INTO {schema}.{SqlConstants.TimeoutMigrationStagingTable} VALUES('headers', NULL, DATEADD(DAY, 1, GETDATE()))";
+            await using var command = new SqlCommand(sql, connection)
+            {
+                CommandType = CommandType.Text
+            };
+            await command.ExecuteNonQueryAsync();
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await sut.Complete(ExistingEndpointName);
+            });
         }
     }
 }
