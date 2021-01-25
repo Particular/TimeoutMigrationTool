0417c7806bb32dcac8690efac9b274a66aa70363
danielmarbach <daniel.marbach@openplace.net>
2021-01-25 09:10:46 +0100

    Align endpoint name length with source

--- src/TimeoutMigrationTool/NHibernate/MigrationsEntityMap.cs
+++ src/TimeoutMigrationTool/NHibernate/MigrationsEntityMap.cs
@@ -8,7 +8,7 @@ namespace Particular.TimeoutMigrationTool.NHibernate
         public MigrationsEntityMap()
         {
             Id(x => x.MigrationRunId);
-            Property(p => p.EndpointName, pm => { pm.Length(500); });
+            Property(p => p.EndpointName, pm => { pm.Length(440); });
             Property(p => p.Status, pm => pm.Type<EnumType<MigrationStatus>>());
             Property(p => p.RunParameters, pm => { pm.Length(4001); });
             Property(p => p.NumberOfBatches);
