78ae5374dcbb7c2eeb28bf224cc6023872d2f4e3
David Boike <david.boike@gmail.com>
2021-06-22 10:24:28 -0500

    Env var was used in more than one place

--- src/TimeoutMigrationTool.SqlP.AcceptanceTests/SqlPToASQEndToEnd.cs
+++ src/TimeoutMigrationTool.SqlP.AcceptanceTests/SqlPToASQEndToEnd.cs
@@ -14,7 +14,7 @@
     [TestFixture]
     class SqlPToASQEndToEnd : SqlPAcceptanceTest
     {
-        string asqConnectionString = Environment.GetEnvironmentVariable("AzureStorageQueue_ConnectionString") ?? "UseDevelopmentStorage=true";
+        string asqConnectionString = Environment.GetEnvironmentVariable("AzureStorage_ConnectionString") ?? "UseDevelopmentStorage=true";
 
         [Test]
         public async Task Can_migrate_timeouts()
